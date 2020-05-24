bd06d1808f4fb565ce8e1ba89693320862b16315
Richard Russell <richardr39@gmail.com>
2020-05-24 21:55:08 +0100

    chore: update package dependencies to work with Java 11

--- project.clj
+++ project.clj
@@ -14,12 +14,12 @@
                           :pretty-print true
                           :source-map true}}]}
 
-  :plugins [[lein-cljsbuild "1.1.4"]
-            [lein-figwheel "0.5.8"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [lein-figwheel "0.5.20"]]
 
   :figwheel {:css-dirs ["resources/public/stylesheets"]}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [reagent "0.6.0"]
-                 [org.clojure/core.async "0.2.395"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.764"]
+                 [reagent "0.10.0"]
+                 [org.clojure/core.async "1.2.603"]])
